eeb976b4236a40ca5dcf34ecb2d7061cc36eb384
Qeti Khetsuriani <ketideveloper@gmail.com>
2022-11-26 21:27:26 +0400

    auth user

--- src/user/dto/update-user.dto.ts
+++ src/user/dto/update-user.dto.ts
@@ -3,6 +3,10 @@ import { IsString, IsEmail } from 'class-validator';
 export class UpdateUserDto {
   @IsString()
   name: string;
+
   @IsEmail()
   email: string;
+
+  @IsString()
+  password: string;
 }
